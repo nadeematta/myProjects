575e07465a6ed6b494c5f78b5dd0c28f7c085ede
unknown <nadeematta@hotmail.com>
2020-01-13 16:43:06 +0500

    v4

--- myScript.sh
+++ myScript.sh
@@ -1,8 +1,10 @@
-if docker ps --format '{{.Names}}' | grep -w myimagecontainer &> /dev/null
+if docker ps --format '{{.Names}}' -a | grep -w myimagecontainer &> /dev/null
 then
-echo container exist...removing container
-docker container rm -f  myimagecontainer
+        echo container exist...removing container
+        docker container rm -f  myimagecontainer
+        docker container run -p 8085:80 -itd --name myimagecontainer myimage
 else
-echo container does not exist
-docker container run -p 8085:80 -itd --name myimagecontainer myimage
+        echo container does not exist
+        docker container run -p 8085:80 -itd --name myimagecontainer myimage
 fi
+
